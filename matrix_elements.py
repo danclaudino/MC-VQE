87291fddaa5bdd66a2213cf824f8df3dd8906dae
Chaves Claudino, Daniel <claudinodc@ornl.gov>
2020-03-18 09:06:30 -0400

    Two-body elements can now loop over all other centers

--- matrix_elements.py
+++ matrix_elements.py
@@ -1,15 +1,16 @@
-import numpy as np
+mport numpy as np
+
+# All equations here refer to the Supplemental Material for PRL 122, 230401, 2019
 
 # Computing two-body Hamiltonian matrix elements
 # using com and com_distance as the global variables that they are
-def two_body_hamiltonian(mu_A, mu_B):
-    V_AB = np.zeros(mu_A.shape[0])
-    for i in range(mu_A.shape[0]):
-        n_AB = (com[i-1]-com[i])/np.linalg.norm(com[i-1]-com[i]) # normal vector along A-B
-        V_AB[i] = np.inner(mu_A[i-1, :], mu_B[i, :]) # mu_A . mu_B
-        V_AB[i] -= 3*np.inner(mu_A[i-1, :], n_AB)*np.inner(mu_B[i, :], n_AB) # -3*(mu_A.n_AB)(mu_B.n_AB)
-        V_AB[i] /= com_distance[i-1,i]**3 # /r_AB**3
-    return V_AB
+def two_body_h(mu_A, mu_B, r_AB):
+        d_AB = np.linalg.norm(r_AB)
+        n_AB = r_AB/d_AB # normal vector along A-B
+        V_AB = np.inner(mu_A, mu_B) # mu_A . mu_B
+        V_AB -= 3*np.inner(mu_A, n_AB)*np.inner(mu_B, n_AB) # -3*(mu_A.n_AB)(mu_B.n_AB)
+        V_AB /= d_AB**3 # / |r_AB|**3
+        return V_AB
 
 # read info from TeraChem outputs
 gs_energies = np.zeros(18) # ground state monomer energy
@@ -18,7 +19,6 @@ com = np.zeros([18, 3]) # coordinates of the center of mass
 gs_dipole = np.zeros([18, 3]) # ground state dipole moment
 es_dipole = np.zeros([18, 3]) # excited state dipole moment
 t_dipole = np.zeros([18, 3]) # transition dipole moment
-
 for outfile in range(18):
 
     filename = '../packet/classical/' + str(outfile + 1) + '/out'
@@ -44,32 +44,63 @@ for outfile in range(18):
             line += 1
 # end of TeraChem output reading
 
-# COM matrix
-com_distance = np.zeros([18,18])
-for i in range(17):
-    for j in range(i+1, 18):
-        com_distance[i, j] = np.linalg.norm((com[i] - com[j])*1.8897161646320724)
-        com_distance[j, i] = np.linalg.norm((com[i] - com[j])*1.8897161646320724)
+# Angstrom to bohr
+com = com*1.8897161646320724
 
-# Dipole momment for the same state are in D and transition dipole moments are in au
+# Dipole momments for the same state are in D and transition dipole moments are in au
 gs_dipole *= 0.393430307
 es_dipole *= 0.393430307
 
-ref = np.load('../packet/hamiltonian/exciton.npz')
-r = ref['R']
-x = ref['X']
-y = ref['Y']
-z = ref['Z']
-
 # One-body Hamiltonian matrix elements
-# S_A (Eq. 20), D_A (Eq. 21)
+# S_A (Eq. 20)
+# D_A (Eq. 21)
 S_A = (gs_energies + es_energies)/2
 D_A = (gs_energies - es_energies)/2
 
+# Two-body Hamiltonian matrix elements
+# (SA|SB) = (O_A 0_A + 1_A 1_A | O_B 0_B + 1_B 1_B)/4 (Eq. 28)
+# (DA|SB) = (O_A 0_A - 1_A 1_A | O_B 0_B + 1_B 1_B)/4 (Eq. 30)
+# (TA|SB) = (O_A 1_A | O_B 0_B + 1_B 1_B)/2 (Eq. 24)
+
+# Diagonal elements of the Hamiltonian (Eq. 13)
+# E = sum_A SA + sum_A>B (SA|SB)
+E = np.sum(S_A)
+for A in range(17):
+    mu_A = (gs_dipole[A, :] + es_dipole[A, :])/2
+    for B in range(A + 1, 18):
+        mu_B = (gs_dipole[B, :] + es_dipole[B, :])/2
+        E += two_body_h(mu_A, mu_B, com[A] - com[B])
+
+# One-body Hamiltonian matrix elements
+# Z_A = D_A + sum_B (DA|SB) (Eq. 14)
+Z_A = np.zeros(18)
+for A in range(18):
+    mu_A = (gs_dipole[A, :] - es_dipole[A, :])/2
+    Z_A[A] = D_A[A]
+    for B in [x for x in range(18) if x != A]:
+        mu_B = (gs_dipole[B, :] + es_dipole[B, :])/2
+        Z_A[A] += two_body_h(mu_A, mu_B, com[A] - com[B])
+
+# X_A = sum_B (TA|SB) (Eq. 15)
+# X_A on the rhs is zero
+X_A = np.zeros(18)
+for A in range(18):
+    mu_A = t_dipole[A, :]
+    for B in [x for x in range(18) if x != A]:
+        mu_B = (gs_dipole[B, :] + es_dipole[B, :])/2
+        X_A[A] += two_body_h(mu_A, mu_B, com[A] - com[B])
+
 # XX_AB = (TA|TB) = (0_A 1_A | O_B 1_B) (Eq. 23)
 # XZ_AB = (TA|DB) = (0_A 1_A | O_B 0_B - 1_B 1_B)/2 (Eq. 25)
-# ZZ_AB = (DA|TB) = (O_A 0_A - 1_A 1_A | 0_B 1_B)/2 (Eq. 25)
-XX = two_body_hamiltonian(t_dipole, t_dipole)
-XZ = two_body_hamiltonian(t_dipole, (gs_dipole - es_dipole)/2)
-ZX = two_body_hamiltonian((gs_dipole - es_dipole)/2, t_dipole)
-ZZ = two_body_hamiltonian((gs_dipole - es_dipole)/2, (gs_dipole - es_dipole)/2)
+# ZX_AB = (DA|TB) = (O_A 0_A - 1_A 1_A | 0_B 1_B)/2 (Eq. 27)
+# ZZ_AB = (DA|DB) = (O_A 0_A - 1_A 1_A | O_B 0_B - 1_B 1_B)/4 (Eq. 31)
+XX_AB = np.zeros([18, 18])
+XZ_AB = np.zeros([18, 18])
+ZX_AB = np.zeros([18, 18])
+ZZ_AB = np.zeros([18, 18])
+for A in range(17):
+    for B in range(A + 1, 18):
+        XX_AB[A, B] = two_body_h(t_dipole[A, :], t_dipole[B, :], com[A] - com[B])
+        XZ_AB[A, B] = two_body_h(t_dipole[A, :], (gs_dipole[B, :] - es_dipole[B, :])/2, com[A] - com[B])
+        ZX_AB[A, B] = two_body_h((gs_dipole[A, :] - es_dipole[A, :])/2, t_dipole[B, :], com[A] - com[B])
+        ZZ_AB[A, B] = two_body_h((gs_dipole[A, :] - es_dipole[A, :])/2, (gs_dipole[B, :] - es_dipole[B, :])/2, com[A] - com[B])
